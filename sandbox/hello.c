33ad5b84f623a31b208856f30f44019e0562acda
anas <anas.chahdi@etu.univ-lyon1.fr>
2024-09-13 13:11:50 +0200

    Test

--- sandbox/hello.c
+++ sandbox/hello.c
@@ -1,15 +1,15 @@
 /* Chacun ajoute son nom ici */
-/* Auteurs : ... et ... */
+/* Auteurs : Leticia Ghilas et Anas Chahdi */
 
 #include <stdio.h>
 
 int main (void) {
 	/* Oups, on a oublié le \n final. */
-	printf("Hello, world");
+	printf("Hello, world\n");
 
 	printf("Je calcule la réponse ... ");
 	
 	/* Oups, on s'est trompé, pour afficher un nombre, c'est
 	   plutôt %d */
-	printf("La réponse est %s\n", 42);
+	printf("La réponse est %d\n", 42);
 }
