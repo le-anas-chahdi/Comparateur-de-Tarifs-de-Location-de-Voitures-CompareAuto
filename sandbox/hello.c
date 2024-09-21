1c6760c69feeadd909c7deb43da5c22742b42f06
Anas Chahdi <anas.chahdi@etu.univ-lyon1.fr>
2024-09-21 18:09:42 +0200

    Update

--- sandbox/hello.c
+++ sandbox/hello.c
@@ -6,7 +6,7 @@ int main (void) {
 	/* Oups, on a oublié le \n final. */
 	printf("Hello, world \n");
 
-	printf("Je calcule la réponse ... ");
+	printf("Je calcule la réponse ...\n");
 	
 	/* Oups, on s'est trompé, pour afficher un nombre, c'est
 	   plutôt %d */
