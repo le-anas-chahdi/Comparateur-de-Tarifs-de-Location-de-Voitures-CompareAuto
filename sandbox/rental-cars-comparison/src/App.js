7227d02c2b4bbbedfab12c110b5a2d45a7e19153
Anas Chahdi <anas.chahdi@etu.univ-lyon1.fr>
2024-09-21 18:34:08 +0200

    Affichage initial

---
+++ sandbox/rental-cars-comparison/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Rental Cars Comparison</h1>
+      <p className="loading-text">Loading...</p>
+    </div>
+  );
+}
+
+export default App;
