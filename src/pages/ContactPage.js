cf38cc2f85ab20c140d6dd62c4462a1cb42ec3b5
ARBADJI MASSY p2210587 <massy.arbadji@etu.univ-lyon1.fr>
2024-10-18 13:36:39 +0000

    Upload New File

---
+++ src/pages/ContactPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import ComparisonForm from '../components/Contact';
+import Contact from '../components/Contact';
+
+const ContactPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Contact Page</h1>
+      <Contact />
+    </div>
+  );
+};
+
+export default ContactPage;
