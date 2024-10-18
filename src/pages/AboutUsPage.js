00ec81c9f97600aae203fb1b4bee1b4b9fd78e5a
ARBADJI MASSY p2210587 <massy.arbadji@etu.univ-lyon1.fr>
2024-10-18 13:36:05 +0000

    Upload New File

---
+++ src/pages/AboutUsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import AboutUs from '../components/AboutUs';
+
+const AboutUsPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>About us</h1>
+      <AboutUs />
+    </div>
+  );
+};
+
+export default AboutUsPage;
