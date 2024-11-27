fc9ab120452c33877a01a355af633d12c9dfdab7
ARBADJI MASSY p2210587 <massy.arbadji@etu.univ-lyon1.fr>
2024-11-27 02:25:47 +0000

    Update PrivacyPolicyPage.js

--- src/pages/public/PrivacyPolicyPage.js
+++ src/pages/public/PrivacyPolicyPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PrivacyPolicy from '../../components/sections/PrivacyPolicy';
+import Navbar from '../../components/layout/Navbar';
 import Footer from '../../components/layout/Footer'; // Assurez-vous que le chemin est correct
 
 const PrivacyPolicyPage = () => {
   return (
     <div className="privacy-policy-page">
+      <Navbar />
       <PrivacyPolicy />
       <Footer />
     </div>
